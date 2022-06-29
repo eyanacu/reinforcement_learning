e760114c45162587ea6bc08e8d18285c682c3be0
eyanacu <108021056+eyanacu@users.noreply.github.com>
2022-06-29 16:45:14 -0500

    Update experiment1.py
    adding execution time

--- examples/python/experiment1.py
+++ examples/python/experiment1.py
@@ -1,180 +1,95 @@
 #!/usr/bin/env python3
 
 #####################################################################
-# This script presents how to use the most basic features of the environment.
-# It configures the engine, and makes the agent perform random actions.
-# It also gets current state and reward earned with the action.
-# <episodes> number of episodes are played. 
+# This script presents how to make use of game variables to implement
+# shaping using health_guided.wad scenario
+# Health_guided scenario is just like health_gathering
+# (see "../../scenarios/README.md") but for each collected medkit global
+# variable number 1 in acs script (coresponding to USER1) is increased
+# by 100.0. It is not considered a part of reward but will possibly
+# reduce learning time.
+# <episodes> number of episodes are played.
 # Random combination of buttons is chosen for every action.
 # Game variables from state and last reward are printed.
-#
-# To see the scenario description go to "../../scenarios/README.md"
 #####################################################################
 
+from argparse import ArgumentParser
 import os
+import itertools as it
 from random import choice
 from time import sleep
+import time
 import vizdoom as vzd
 
+DEFAULT_CONFIG = os.path.join(vzd.scenarios_path, "health_gathering_supreme.cfg")
 
 if __name__ == "__main__":
-    # Create DoomGame instance. It will run the game and communicate with you.
+    parser = ArgumentParser("ViZDoom example showing how to use shaping for health gathering scenario.")
+    parser.add_argument(dest="config",
+                        default=DEFAULT_CONFIG,
+                        nargs="?",
+                        help="Path to the configuration file of the scenario."
+                             " Please see "
+                             "../../scenarios/*cfg for more scenarios.")
+
+    args = parser.parse_args()
+
     game = vzd.DoomGame()
 
-    # Now it's time for configuration!
-    # load_config could be used to load configuration instead of doing it here with code.
-    # If load_config is used in-code configuration will also work - most recent changes will add to previous ones.
-    # game.load_config("../../scenarios/basic.cfg")
+    # Choose scenario config file you wish to watch.
+    # Don't load two configs cause the second will overrite the first one.
+    # Multiple config files are ok but combining these ones doesn't make much sense.
 
-    # Sets path to additional resources wad file which is basically your scenario wad.
-    # If not specified default maps will be used and it's pretty much useless... unless you want to play good old Doom.
-    game.set_doom_scenario_path(os.path.join(vzd.scenarios_path, "basic.wad"))
-
-    # Sets map to start (scenario .wad files can contain many maps).
-    game.set_doom_map("map01")
-
-    # Sets resolution. Default is 320X240
+    game.load_config(args.config)
     game.set_screen_resolution(vzd.ScreenResolution.RES_640X480)
 
-    # Sets the screen buffer format. Not used here but now you can change it. Default is CRCGCB.
-    game.set_screen_format(vzd.ScreenFormat.RGB24)
-
-    # Enables depth buffer.
-    game.set_depth_buffer_enabled(True)
-
-    # Enables labeling of in game objects labeling.
-    game.set_labels_buffer_enabled(True)
-
-    # Enables buffer with top down map of the current episode/level.
-    game.set_automap_buffer_enabled(True)
-
-    # Enables information about all objects present in the current episode/level.
-    game.set_objects_info_enabled(True)
-
-    # Enables information about all sectors (map layout).
-    game.set_sectors_info_enabled(True)
-
-    # Sets other rendering options (all of these options except crosshair are enabled (set to True) by default)
-    game.set_render_hud(False)
-    game.set_render_minimal_hud(False)  # If hud is enabled
-    game.set_render_crosshair(False)
-    game.set_render_weapon(True)
-    game.set_render_decals(False)  # Bullet holes and blood on the walls
-    game.set_render_particles(False)
-    game.set_render_effects_sprites(False)  # Smoke and blood
-    game.set_render_messages(False)  # In-game messages
-    game.set_render_corpses(False)
-    game.set_render_screen_flashes(True)  # Effect upon taking damage or picking up items
-
-    # Adds buttons that will be allowed to use.
-    # This can be done by adding buttons one by one:
-    # game.clear_available_buttons()
-    # game.add_available_button(vzd.Button.MOVE_LEFT)
-    # game.add_available_button(vzd.Button.MOVE_RIGHT)
-    # game.add_available_button(vzd.Button.ATTACK)
-    # Or by setting them all at once:
-    game.set_available_buttons([vzd.Button.MOVE_LEFT, vzd.Button.MOVE_RIGHT, vzd.Button.ATTACK])
-    # Buttons that will be used can be also checked by:
-    print("Available buttons:", [b.name for b in game.get_available_buttons()])
-
-    # Adds game variables that will be included in state.
-    # Similarly to buttons, they can be added one by one:
-    # game.clear_available_game_variables()
-    # game.add_available_game_variable(vzd.GameVariable.AMMO2)
-    # Or:
-    game.set_available_game_variables([vzd.GameVariable.AMMO2])
-    print("Available game variables:", [v.name for v in game.get_available_game_variables()])
-
-    # Causes episodes to finish after 200 tics (actions)
-    game.set_episode_timeout(200)
-
-    # Makes episodes start after 10 tics (~after raising the weapon)
-    game.set_episode_start_time(10)
-
-    # Makes the window appear (turned on by default)
-    game.set_window_visible(True)
-
-    # Turns on the sound. (turned off by default)
-    # game.set_sound_enabled(True)
-    # Because of some problems with OpenAL on Ubuntu 20.04, we keep this line commented,
-    # the sound is only useful for humans watching the game.
-
-    # Sets the living reward (for each move) to -1
-    game.set_living_reward(-1)
-
-    # Sets ViZDoom mode (PLAYER, ASYNC_PLAYER, SPECTATOR, ASYNC_SPECTATOR, PLAYER mode is default)
-    game.set_mode(vzd.Mode.PLAYER)
-
-    # Enables engine output to console, in case of a problem this might provide additional information.
-    #game.set_console_enabled(True)
-
-    # Initialize the game. Further configuration won't take any effect from now on.
     game.init()
 
-    # Define some actions. Each list entry corresponds to declared buttons:
-    # MOVE_LEFT, MOVE_RIGHT, ATTACK
-    # game.get_available_buttons_size() can be used to check the number of available buttons.
-    # 5 more combinations are naturally possible but only 3 are included for transparency when watching.
-    actions = [[True, False, False], [False, True, False], [False, False, True]]
+    # Creates all possible actions.
+    actions_num = game.get_available_buttons_size()
+    actions = []
+    for perm in it.product([False, True], repeat=actions_num):
+        actions.append(list(perm))
 
-    # Run this many episodes
-    episodes = 10
-
-    # Sets time that will pause the engine after each action (in seconds)
-    # Without this everything would go too fast for you to keep track of what's happening.
-    sleep_time = 1.0 / vzd.DEFAULT_TICRATE  # = 0.028
+    episodes = 20
+    sleep_time = 0.028
+    start_time = time.time()
 
     for i in range(episodes):
-        print("Episode #" + str(i + 1))
 
-        # Starts a new episode. It is not needed right after init() but it doesn't cost much. At least the loop is nicer.
+        print("Episode #" + str(i + 1))
+        # Not needed for the first episode but the loop is nicer.
         game.new_episode()
+
+        # Use this to remember last shaping reward value.
+        last_total_shaping_reward = 0
 
         while not game.is_episode_finished():
 
-            # Gets the state
+            # Gets the state and possibly to something with it
             state = game.get_state()
 
-            # Which consists of:
-            n = state.number
-            vars = state.game_variables
-            screen_buf = state.screen_buffer
-            depth_buf = state.depth_buffer
-            labels_buf = state.labels_buffer
-            automap_buf = state.automap_buffer
-            labels = state.labels
-            objects = state.objects
-            sectors = state.sectors
+            # Makes a random action and save the reward.
+            reward = game.make_action(choice(actions))
 
-            # Games variables can be also accessed via
-            # (including the ones that were not added as available to a game state):
-            #game.get_game_variable(GameVariable.AMMO2)
+            # Retrieve the shaping reward
+            fixed_shaping_reward = game.get_game_variable(vzd.GameVariable.USER1)  # Get value of scripted variable
+            shaping_reward = vzd.doom_fixed_to_double(
+                fixed_shaping_reward)  # If value is in DoomFixed format project it to double
+            shaping_reward = shaping_reward - last_total_shaping_reward
+            last_total_shaping_reward += shaping_reward
 
-            # Makes an action (here random one) and returns a reward.
-            r = game.make_action(choice(actions))
-
-            # Makes a "prolonged" action and skip frames:
-            # skiprate = 4
-            # r = game.make_action(choice(actions), skiprate)
-
-            # The same could be achieved with:
-            # game.set_action(choice(actions))
-            # game.advance_action(skiprate)
-            # r = game.get_last_reward()
-
-            # Prints state's game variables and reward.
-            print("State #" + str(n))
-            print("Game variables:", vars)
-            print("Reward:", r)
+            print("State #" + str(state.number))
+            print("Health: ", state.game_variables[0])
+            print("Last Reward:", reward)
+            print("Last Shaping Reward:", shaping_reward)
             print("=====================")
 
+            # Sleep some time because processing is too fast to watch.
             if sleep_time > 0:
                 sleep(sleep_time)
 
-        # Check how the episode went.
-        print("Episode finished.")
+        print("Episode finished!")
         print("Total reward:", game.get_total_reward())
         print("************************")
-
-    # It will be done automatically anyway but sometimes you need to do it in the middle of the program...
-    game.close()
+print("--- %s seconds ---" % (time.time() - start_time))
